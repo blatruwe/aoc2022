dbbaee429c6f12bf3d17aaf0d0a99a48ec19aead
Brecht Latruwe <brecht.latruwe@gmail.com>
2022-12-03 15:25:40 +0100

    day0

---
+++ day1/part1.py
@@ -0,0 +1,20 @@
+f = open("day1/input_part1", "r")
+elf=1
+elf_list=[]
+sum=0
+for line in f : 
+    if(not line.strip()):
+        elf_list.append(sum)
+        sum=0
+    else:
+        sum=sum+int(line.strip())
+elf_list.append(sum)
+
+print( max(elf_list))
+
+
+#part 2
+
+s=sorted(elf_list)
+total=s[-1]+s[-2]+s[-3]
+print(total)
