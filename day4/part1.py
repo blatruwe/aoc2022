6aa3fb1510231e40a956a3816e0a4391389267a2
Brecht Latruwe <brecht.latruwe@gmail.com>
2022-12-04 16:03:26 +0100

    day4 optimalisation

--- day4/part1.py
+++ day4/part1.py
@@ -2,19 +2,6 @@
 sum=0
 f=open("day4/input")
 for line in f:
-    assignment1_list=list(range(int(line.split(',')[0].split('-')[0]),int(line.split(',')[0].split('-')[1])+1))
-    assignment2_list=list(range(int(line.strip().split(',')[1].split('-')[0]),int(line.strip().split(',')[1].split('-')[1])+1))
-    all_in_list=True
-    for i in assignment1_list:
-        if not i in assignment2_list:
-             all_in_list=False
-    if all_in_list:
-        sum=sum+1
-        continue
-    all_in_list=True
-    for i in assignment2_list:
-        if not i in assignment1_list:
-             all_in_list=False
-    if all_in_list:
+    if (set(list(range(int(line.split(',')[0].split('-')[0]),int(line.split(',')[0].split('-')[1])+1))).issubset(set(list(range(int(line.strip().split(',')[1].split('-')[0]),int(line.strip().split(',')[1].split('-')[1])+1)))) or set(list(range(int(line.strip().split(',')[1].split('-')[0]),int(line.strip().split(',')[1].split('-')[1])+1))).issubset(set(list(range(int(line.split(',')[0].split('-')[0]),int(line.split(',')[0].split('-')[1])+1))))):
         sum=sum+1
 print(sum)
