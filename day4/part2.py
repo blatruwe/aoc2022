76c1c9ae30e08be523da363645f6ab6e572cc075
Brecht Latruwe <brecht.latruwe@gmail.com>
2022-12-04 15:50:09 +0100

    day4 optimalisation

--- day4/part2.py
+++ day4/part2.py
@@ -2,18 +2,6 @@
 sum=0
 f=open("day4/input")
 for line in f:
-    assignment1_list=list(range(int(line.split(',')[0].split('-')[0]),int(line.split(',')[0].split('-')[1])+1))
-    assignment2_list=list(range(int(line.strip().split(',')[1].split('-')[0]),int(line.strip().split(',')[1].split('-')[1])+1))
-    overlap=False
-    for i in assignment1_list:
-        if i in assignment2_list:
-            overlap=True
-            break
-        if(overlap):
-            break
-    if (overlap):         
+    if list(set(list(range(int(line.split(',')[0].split('-')[0]),int(line.split(',')[0].split('-')[1])+1)))&set(list(range(int(line.strip().split(',')[1].split('-')[0]),int(line.strip().split(',')[1].split('-')[1])+1)))):
         sum=sum+1
-             
-
-
 print(sum)
