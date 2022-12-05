02a4e8d453720310c5f2cb0740f720b45f5858dd
Brecht Latruwe <brecht.latruwe@gmail.com>
2022-12-05 21:46:52 +0100

    cleanup

--- day3/part2.py
+++ day3/part2.py
@@ -1,17 +1,14 @@
 f=open("day3/input")  
 priority=0
 i=0
-elf1=""
-elf2=""
-elf3=""
 priority=0
 for line in f:
     i=i+1
     if(i==1):
         elf1=line.strip()
-    if(i==2):
+    elif(i==2):
         elf2=line.strip()
-    if(i==3):
+    elif(i==3):
         elf3=line.strip()
         common = set.intersection(*map(set,(elf1,elf2,elf3)))
         common_letter=str(common.pop())
